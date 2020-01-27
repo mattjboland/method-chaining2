f1a1ea7b2ffbd07d2aeb224e4a084d1c807a05a9
mattjboland <mattjamesboland@gmail.com>
2020-01-27 16:47:19 +0000

    adding mouseenter/mouseleave in script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
- 
-
+    $("#button1").mouseenter(function(){
+        $('#button1').removeClass("makeRed").addClass("makeBorder");
+    });
+    $("#button1").mouseleave(function(){
+        $("#button1").removeClass("makeBorder").addClass("makeRed");
+    });
 
 
 }); 
