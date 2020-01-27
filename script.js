15992af2fc00e9f7aedd12a5d2223d8ae640187a
mattjboland <mattjamesboland@gmail.com>
2020-01-27 17:35:31 +0000

    update script.js with hide/show fadeIn/fadeOut

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-    $("#button1").mouseenter(function(){
-        $('#button1').removeClass("makeRed").addClass("makeBorder");
+    $("button").click(function(){
+        $("p").hide(2000).show(2000);
     });
-    $("#button1").mouseleave(function(){
-        $("#button1").removeClass("makeBorder").addClass("makeRed");
+    $("button").click(function(){
+        $("p").fadeIn().fadeOut();
     });
-
 
 }); 
